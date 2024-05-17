8dd20d9c44efbc7136496261410858fc281d82ed
adgoetz186 <agoetz137@gmail.com>
2024-05-17 12:29:25 -0400

    Added sampling code

---
+++ Programs/Flux_Analysis/Sampling/recon_1_t_cells_gene_integration_12_11_23/gene_HR_warmup_all_mice.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import scipy.io as sio
+
+# Finds random vertices in polytope for HR sampling, see readme.txt for suggestions for running
+
+# _____ Setting the CWD to be Flux_Code BEGIN _____
+# Flux_Code path here
+path_to_FC = ""
+if path_to_FC == "":
+	try:
+		# Obtains the location of the Cell_signaling_information folder if it is in the cwd parents
+		path_to_FC = Path.cwd().parents[[Path.cwd().parents[i].parts[-1] for i in range(len(Path.cwd().parts) - 1)].index(
+			"Flux_Code")]
+	except ValueError:
+		print("Flux_Code not found in cwd parents, trying sys.path")
+		try:
+			# Obtains the location of the Cell_signaling_information folder if it is in sys.path
+			path_to_CSI = Path(sys.path[[Path(i).parts[-1] for i in sys.path].index("Flux_Code")])
+		except ValueError:
+			print("Flux_Code not found in sys.path "
+			      "consult 'Errors with setting working directory' in README")
+else:
+	path_to_CSI = Path(path_to_FC)
+os.chdir(path_to_FC)
+from Programs.Flux_Analysis.Classes_And_Functions.Flux_Model_Class import Flux_Balance_Model
+# _____ Setting the CWD to be Flux_Code END _____
+
+# Remove
+Internal_nullspace_S = sio.loadmat(Path("Data/null_space_S/recon_1_t_cells_12_11_23/NS.mat"))['NS']
+trans_NS = np.transpose(Internal_nullspace_S)
+
+
+positive_min_model_location = Path("Data/Models/json_models/fast_key_format/recon_1_t_cells_12_11_23/edited_pos_min_model_Day_2_single_size")
+# it should be possible to eventually just have HR ready models after exp_aligned
+# This way breaks things up to save intermediates
+HR_ready_model_location = Path("Data/Models/json_models/fast_key_format/recon_1_t_cells_12_11_23/HR_Ready_Day_2_single_size")
+warmup_sample_location = Path("Data/HR/HR_Warmup/recon_1_t_cells_gene_Day_2_single_size_12_11_23")
+
+flux_model_dict = {}
+for filename in os.listdir(positive_min_model_location):
+	print(filename)
+	recon_flux_model = Flux_Balance_Model()
+	recon_flux_model.load_fast_key_json_model(positive_min_model_location / filename)
+	flux_model_dict[recon_flux_model.model_dict["model_name"]] = recon_flux_model
+
+print(flux_model_dict)
+
+for flux_model_name in flux_model_dict.keys():
+	print(flux_model_name)
+	flux_model = flux_model_dict[flux_model_name]
+	# 10000
+	# 5
+	warmup = flux_model.generate_warmup_gb_pinch(10000,[1,1.005,1.01], repeat_basis= 5,points_save_path = warmup_sample_location,model_save_path = HR_ready_model_location,trans_NS = trans_NS)
